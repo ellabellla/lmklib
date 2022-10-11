829563e9f7da0c46a4dbe4badab7da7690bbb49e
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-10-11 21:57:23 +1100

    + fix open

--- lmk-kout/src/main.rs
+++ lmk-kout/src/main.rs
@@ -1,11 +1,14 @@
-use std::{time::Duration, thread, fs::File};
+use std::{time::Duration, thread, fs::OpenOptions, os::unix::prelude::OpenOptionsExt};
 
 use lmk_hid::key;
 
 fn main() {
     thread::sleep(Duration::from_secs(1));
 
-    let mut hid = File::open("/dev/hidg0").unwrap();
+    let mut hid = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("/dev/hidg0").unwrap();
     let packets = key::string_to_packets("Hello, world!");
     key::send_key_packets(&packets, &mut hid).unwrap();
 }
