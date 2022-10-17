7aa88e952be7bd761b1b87a947a758520b5e4a1a
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-10-17 15:07:38 +1100

    + load & run script

--- lmk-quack/src/main.rs
+++ lmk-quack/src/main.rs
@@ -1,6 +1,34 @@
+use std::{fs};
+
+use clap::Parser;
+use interpreter::QuackInterp;
+use lmk_hid::HID;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Input script
+    input: String,
+}
+
 mod parser;
 mod interpreter;
 
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let input = fs::read_to_string(args.input).unwrap();
+
+
+    let mut hid = HID::new(1, 0);
+    let interpreter = QuackInterp::new(&input);
+    interpreter.run(&mut hid);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    pub fn test() {
+    }
 }
