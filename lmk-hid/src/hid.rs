29c1e4b5662eb0ca9dfefb2abdfea006bed0a9bb
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-10-18 14:06:05 +1100

    + debug feature

--- lmk-hid/src/hid.rs
+++ lmk-hid/src/hid.rs
@@ -1,28 +1,62 @@
-use std::{fs::{OpenOptions, File}, io::{Write, self}};
 
-pub struct HID {
-    mouse_hid: File,
-    keyboard_hid: File,
+pub use hid::HID;
+
+#[cfg(not(feature = "debug"))]
+mod hid {
+    use std::{fs::{OpenOptions, File}, io::{Write, self}};
+
+    pub struct HID {
+        mouse_hid: File,
+        keyboard_hid: File,
+    }
+    
+    impl HID {
+        pub fn new(mouse_id: u8, keyboard_id: u8) -> io::Result<HID>{
+            Ok(HID {
+                mouse_hid: OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .open(format!("/dev/hidg{}", mouse_id))?, 
+                keyboard_hid: OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .open(format!("/dev/hidg{}", keyboard_id))? })
+        }
+    
+        pub fn send_key_packet(&mut self, data: &[u8]) -> io::Result<usize> {
+            self.keyboard_hid.write(data)
+        }
+    
+        pub fn send_mouse_packet(&mut self, data: &[u8]) -> io::Result<usize> {
+            self.mouse_hid.write(data)
+        }
+    }
+    
 }
+#[cfg(feature = "debug")]
+mod hid {
+    use std::io;
 
-impl HID {
-    pub fn new(mouse_id: u8, keyboard_id: u8) -> io::Result<HID>{
-        Ok(HID {
-            mouse_hid: OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(format!("/dev/hidg{}", mouse_id))?, 
-            keyboard_hid: OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(format!("/dev/hidg{}", keyboard_id))? })
+    use crate::key::KeyPacket;
+
+    pub struct HID {
     }
-
-    pub fn send_key_packet(&mut self, data: &[u8]) -> io::Result<usize> {
-        self.keyboard_hid.write(data)
-    }
-
-    pub fn send_mouse_packet(&mut self, data: &[u8]) -> io::Result<usize> {
-        self.mouse_hid.write(data)
+    
+    impl HID {
+        pub fn new(_mouse_id: u8, _keyboard_id: u8) -> io::Result<HID>{
+            Ok(HID {})
+        }
+    
+        pub fn send_key_packet(&mut self, data: &[u8]) -> io::Result<usize> {
+            print!("SEND KEY: ");
+            KeyPacket::print_data(data);
+            Ok(data.len())
+        }
+    
+        pub fn send_mouse_packet(&mut self, data: &[u8]) -> io::Result<usize> {
+            print!("SEND MOUSE: ");
+            KeyPacket::print_data(data);
+            Ok(data.len())
+        }
     }
 }
