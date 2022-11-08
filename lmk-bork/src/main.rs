65e479233ff3bf9cbb758a7050d0f6b4be139823
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-11-08 16:13:44 +1100

    + main

--- lmk-bork/src/main.rs
+++ lmk-bork/src/main.rs
@@ -1,5 +1,27 @@
+use std::fs::{self};
+
+use clap::{Parser};
+use interpreter::BorkInterp;
+
 mod parser;
+mod interpreter;
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Input script
+    input: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let source = match fs::read_to_string(&args.input){
+        Ok(source) => source,
+        Err(_) => {
+            println!("Could not open file '{}'.", args.input);
+            return;
+        },
+    };
+    let borker = BorkInterp::new(&source);
+
+    borker.run().unwrap();    
 }
