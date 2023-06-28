14937816b3900d2cfbf6a89375c0e0044d0d569b
ellabellla <86757313+ellabellla@users.noreply.github.com>
2023-06-28 15:30:07 +1000

    + generalize path

--- bin/hid-interface/install-service.sh
+++ bin/hid-interface/install-service.sh
@@ -1,5 +1,5 @@
-mkdir -p /home/ella/.config/systemd/user
-cp hid.service /home/ella/.config/systemd/user/hid.service
+mkdir -p $HOME/.config/systemd/user
+cp hid.service $HOME/.config/systemd/user/hid.service
 systemctl --user daemon-reload
 systemctl --user enable hid.service
 systemctl --user start hid.service
